a8023e7b111ff0ac00ed7ef1b81fc9a42cd40c03
Gabriel Zerbib <gabrielzerbib@users.noreply.github.com>
2015-04-29 16:26:25 +0300

    Create parasha_loc.js

---
+++ calj/parasha_loc.js
@@ -0,0 +1,81 @@
+
+/***************************************
+ *            parasha_loc.js           *
+ ***************************************
+ * Human-readable names of parshiot    *
+ *    English                          *
+ ***************************************
+ * Requires: jdates.js, parasha.js     *
+ ***************************************
+ * January 5, 2005 - Tevet 24, 5765    *
+ *  v1.0                               *
+ *                                     *
+ * (c) Gabriel Zerbib,                 *
+ *   gabriel@bumpt.net                 *
+ *   http://www.bumpt.net              *
+ *                                     *
+ * It is strictly forbidden to use or  *
+ * reproduce all or parts of this      *
+ * program without author's explicit   *
+ * permission.                         *
+ * Commercial use of this program is   *
+ * subject to purchase. Please contact *
+ * the author.                         *
+ ***************************************/
+
+JDate.parshiot = [
+	"BERESHIT",
+	"NOACH",
+	"LECH L'CHA",
+	"VAYERA",
+	"CHAYEI SARAH",
+	"TOL'DOT",
+	"VAYETSE",
+	"VAYISHLACH",
+	"VAYESHEV",
+	"MIKETZ",
+	"VAYIGASH",
+	"VAY'CHI",
+	"SHEMOT",
+	"VA'ERA",
+	"BO",
+	"BESHALA'H",
+	"YITRO",
+	"MISHPATIM",
+	"TEROUMA",
+	"TETSAVEH",
+	"KI TISSA",
+	"VAYAQ'HEL",
+	"PEKUDEI",
+	"VAYIKRA",
+	"TSAV",
+	"SHEMINI",
+	"TAZRIA",
+	"METSORA",
+	"AHHAREI MOT",
+	"QEDOSHIM",
+	"EMOR",
+	"BEHAR",
+	"BEHHOUKOTAI",
+	"BAMIDBAR",
+	"NASSO",
+	"BEHA'ALOTKHA",
+	"SHELAHH LEKHA",
+	"KORAHH",
+	"HHOUKAT",
+	"BALAK",
+	"PINHHAS",
+	"MATOT",
+	"MASS'EI",
+	"DEVARIM",
+	"VAET'HHANAN",
+	"EKEV",
+	"RE'EH",
+	"SHOF'TIM",
+	"KI TETSE",
+	"KI TAVO",
+	"NITSAVIM",
+	"VAYELECH",
+	"HA'AZINU",
+	"VEZOT HABERAKHA"
+];
